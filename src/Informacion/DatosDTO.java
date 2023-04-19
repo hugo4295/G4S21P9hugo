dfa8fd98db83cb73b24cde9d95bbb817254eecf6
hugo4295 <hugo_4295@hotmail.com>
2023-04-18 20:50:35 -0600

    primera parte del manejo de objetos dto

---
+++ src/Informacion/DatosDTO.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Informacion;
+
+/**
+ * DTO -> Data Transfer Object
+ * @author hugo4295
+ */
+public class DatosDTO {
+    String nombre;
+    int edad;
+    String correo;
+
+    public DatosDTO(String nombre, int edad, String correo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.correo = correo;
+    }
+
+    public DatosDTO() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "Datos ->" + nombre + " " + edad + " " + correo;
+    }
+       
+}
---
+++ src/g4s21p9hugo/G4S21P9hugo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g4s21p9hugo;
+
+/**
+ *
+ * @author hugo4295
+ */
+public class G4S21P9hugo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new JFPrincipal().setVisible(true);
+    }
+    
+}
