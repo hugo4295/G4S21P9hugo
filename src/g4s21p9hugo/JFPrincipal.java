0a8e5d07228d4171dd955ba809672e04afdc6e56
tesoem <tesoem@10.15.66.4>
2023-04-21 19:48:51 -0500

    Alumno: Vazquez Lopez Sergio Aaron Actualizacion Branch

--- src/g4s21p9hugo/JFPrincipal.java
+++ src/g4s21p9hugo/JFPrincipal.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package g4s21p9savl;
+package g4s21p9hugo;
 
 import Informacion.DatosDTO;
 import almacen.Archivos;
